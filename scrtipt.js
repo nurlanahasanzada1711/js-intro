9d7fbac32056041b0bacecc325b63a681b998ce5
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-04-11 00:30:52 +0400

    hello

---
+++ scrtipt.js
@@ -0,0 +1,23 @@
+let num = 130;
+let count = 0;
+
+if (num < 50) {
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0) {
+            count++;
+        }
+    }
+} else if (num >= 50 && num <= 100) {
+    for (let i = 1; i <= 50; i++) {
+        if (i % 5 === 0) {
+            count++;
+        }
+    }
+} else if (num > 100) {
+    for (let i = 1; i <= num; i++) {
+        if (i % 8 === 0) {
+            count++;
+        }
+    }
+}
+console.log(`${count}`);
